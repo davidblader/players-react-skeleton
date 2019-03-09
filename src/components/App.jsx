7fecb161bb3d363975a5040ce3fd2d2e1e0a370c
davidblader <dblader79@gmail.com>
2019-03-09 00:40:28 -0500

    trying to get used to pure function components :sweat_smile:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Registration from './Registration';
 import Roster from './Roster';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const App = () => {
-  ({
-  <React.Fragment>
-    <Login/>
-    <Registration/>
-    <Roster/>
-    <div>Replace me with something you can be proud of.</div>
-  </React.Fragment>
-});
-  }
-}
+const App = () =>
+  (
+    <React.Fragment>
+      <Login />
+      <Registration />
+      <Roster />
+      <Router >
+        <Link to="/" />
+        <Route />
+      </Router>
+      <div>Replace me with something you can be proud of.</div>
+    </React.Fragment>
+  );
+
 
 export default App;
