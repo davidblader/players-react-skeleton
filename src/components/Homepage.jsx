cf508fd41e06ca2440c462e6f21d6656de071f76
davidblader <dblader79@gmail.com>
2019-03-10 03:06:33 -0400

    made Login stateful

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import VerticalCenter from './VerticalCenter';
 import AnimalCrossingHeader from './AnimalCrossingHeader';
 import Button from './Button';
@@ -9,14 +9,14 @@ const Homepage = () =>
     <VerticalCenter>
       <div id="homepage">
         <AnimalCrossingHeader>Animal Crossing Fantasy Village</AnimalCrossingHeader>
-          <div>
-            <Link to="/login">
-              <Button>Login</Button>
-            </Link>
-            <Link to="/register">
-              <Button>Move into Town!</Button>
-            </Link>
-          </div>
+        <div className="link-btn">
+          <Link to="/login" href="/login">
+            <Button>Login</Button>
+          </Link>
+          <Link to="/register" href="/register">
+            <Button>Move into Town!</Button>
+          </Link>
+        </div>
       </div>
     </VerticalCenter>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 
-const Login = () =>
-  (
-    <div id="login">
-      <h2>Login</h2>
-      <div id="login-inputs">
-        <form>
-          <div><input placeholder="Email" name="email" type="text" /></div>
-          <div><input placeholder="Password" name="password" type="password" /></div>
-        </form>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginFields: {
+        email: '',
+        password: '',
+      },
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state.loginFields;
+    console.log(email);
+    console.log(password);
+  }
+
+  handleChange(e) {
+    const { loginFields } = this.state;
+    loginFields[e.target.name] = e.target.value;
+    this.setState({ loginFields });
+  }
+
+  render() {
+    return (
+      <div id="login">
+        <h2>Login</h2>
+        <div id="login-inputs">
+          <form onSubmit={this.handleSubmit}>
+            <div><input placeholder="Email" name="email" type="text" onChange={this.handleChange} /></div>
+            <div><input placeholder="Password" name="password" type="password" onChange={this.handleChange} /></div>
+            <div><input type="submit" value="Login" /></div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
 
 export default Login;
