e5f15f5f9575844f8794148a74a8274485b51827
davidblader <dblader79@gmail.com>
2019-03-20 14:47:30 -0400

    remove duplicate react-router-dom import

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { logout } from './App';
 import Button from './Button';
 import AnimalCrossingHeader from './AnimalCrossingHeader';
 import AnimalCrossingContainer from './AnimalCrossingContainer';
 import Loading from './Loading';
-import { Redirect } from 'react-router-dom';
 
 const NoPlayers = () =>
   (
