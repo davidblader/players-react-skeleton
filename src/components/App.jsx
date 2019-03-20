4be9e1bf7b1e3c8d75c5acd56ded9f08cd05ed1a
davidblader <dblader79@gmail.com>
2019-03-20 15:23:08 -0400

    not very good implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,18 +19,56 @@ export const logout = () => {
   window.location.replace('/');
 };
 
-const App = () =>
-  (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/login" render={routerProps => <Login {...routerProps} setSession={setSession} />} />
-        <Route path="/register" render={routerProps => <Registration {...routerProps} setSession={setSession} />} />
-        <PrivateRoute path="/roster" component={props => <Roster {...props} logout={logout} />} />
-        <PrivateRoute path="/player/new" component={props => <NewPlayer {...props} />} />
-        <Route component={NoMatch} />
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+    };
+    this.authenticate = this.authenticate.bind(this);
+    this.authenticateInterval = 10000;
+  }
+
+  componentDidMount() {
+    this.authenticate();
+    setInterval(this.authenticate, this.authenticateInterval);
+  }
+
+  authenticate() {
+    fetch('https://players-api.developer.alchemy.codes/api/players', {
+      headers: { Authorization: `Bearer ${localStorage.getItem('JWT')}` },
+    }).then(resp => resp.json())
+      .then(data => this.setState({ isAuthenticated: data.success }));
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/login" render={routerProps => <Login {...routerProps} setSession={setSession} />} />
+          <Route path="/register" render={routerProps => <Registration {...routerProps} setSession={setSession} />} />
+          <PrivateRoute path="/roster" isAuth={this.state.isAuthenticated} component={props => <Roster {...props} logout={logout} />} />
+          <PrivateRoute path="/player/new" isAuth={this.state.isAuthenticated} component={props => <NewPlayer {...props} />} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+// const App = () =>
+//   (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" component={Homepage} />
+//         <Route path="/login" render={routerProps => <Login {...routerProps} setSession={setSession} />} />
+//         <Route path="/register" render={routerProps => <Registration {...routerProps} setSession={setSession} />} />
+//         <PrivateRoute path="/roster" component={props => <Roster {...props} logout={logout} />} />
+//         <PrivateRoute path="/player/new" component={props => <NewPlayer {...props} />} />
+//         <Route component={NoMatch} />
+//       </Switch>
+//     </Router>
+//   );
 
 export default App;
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ isAuth, component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props => (
-  localStorage.getItem('JWT')
-    ? <Component {...props} />
-    : <Redirect to="/login" />)}
+  isAuth ? <Component {...props} /> : <Redirect to="/login" />)}
   />
 );
 
 PrivateRoute.propTypes = {
   component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+  isAuth: PropTypes.bool.isRequired,
 };
 
 export default PrivateRoute;
